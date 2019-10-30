5cd975ba1c1d181c16d8e011277f6ab7a8d440ae
Takefumi MIYOSHI <miyo@wasamon.net>
2019-10-30 19:08:42 +0900

    start to use hugo

---
+++ docs/search.min.aafafe43dc55b3d0af6c763abd7d75f2ac5cae7860bb1a395205a5d10a9c1a93.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/docs/flexsearch.min.js');loadScript('/docs/search-data.min.4f0c0f8e4a3206071f3f87bb842c2db1f6c03f985e2a864d17308a35fcfa122b.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
