d7f3fd24f124af7d1e962a7b7958e2e753e92d85
Takefumi MIYOSHI <miyo@wasamon.net>
2019-10-31 23:01:20 +0900

    implementing PHI-expr backend

---
+++ docs/search.min.6c6c850a6d055e2052b2f64fd4c9a5c01bcb61f7ffa317944ce5596a4c510008.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/learning_fpga/flexsearch.min.js');loadScript('/learning_fpga/search-data.min.1ea88e5d2d45cbd10fedbca794d0ae720eb947d1af6f5b1e97ab3bd7c8e94bb8.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
