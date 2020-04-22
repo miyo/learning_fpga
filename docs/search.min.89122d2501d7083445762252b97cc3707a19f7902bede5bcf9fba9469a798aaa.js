7b51fd390f147e86dde61f90423a460e110f43b0
Takefumi MIYOSHI <miyo@wasamon.net>
2020-04-22 11:08:15 +0900

    :)

---
+++ docs/search.min.89122d2501d7083445762252b97cc3707a19f7902bede5bcf9fba9469a798aaa.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/learning_fpga/flexsearch.min.js');loadScript('/learning_fpga/search-data.min.65dbc68f8d16db86bb9816e3efd15f36fc0408d36d8b1ae75533267cac148142.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
