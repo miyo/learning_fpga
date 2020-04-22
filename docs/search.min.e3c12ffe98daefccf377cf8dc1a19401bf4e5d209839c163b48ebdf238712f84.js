74a884a8855ce557219002c11926a50576db0a16
Takefumi MIYOSHI <miyo@wasamon.net>
2020-04-22 11:18:37 +0900

    bugfix: sections level

---
+++ docs/search.min.e3c12ffe98daefccf377cf8dc1a19401bf4e5d209839c163b48ebdf238712f84.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/learning_fpga/flexsearch.min.js');loadScript('/learning_fpga/search-data.min.fad776a354a809d55774a1dd14c86287b220c401cc6f962462db88c1470afad1.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
